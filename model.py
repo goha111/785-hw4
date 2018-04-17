ed477b1e5b92153b42182b91f4052b84d08485e7
Hang Gong <gohakido@gmail.com>
2018-04-16 21:43:23 -0400

    VLSTM module finished

--- model.py
+++ model.py
@@ -10,24 +10,28 @@ def initializer(m):
         elif 'bias' in name:
             param.data.zero_()
 
-
-class VInitLSTM(nn.LSTM):
+class VLSTM(nn.LSTM):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         hidden_size = kwargs.get('hidden_size')
-        self.h0 = nn.Parameter(torch.zeros(1, 1, hidden_size))
-        self.c0 = nn.Parameter(torch.zeros(1, 1, hidden_size))
+        bidirectional = kwargs.get('bidirectional')
+        num_layers = kwargs.get('num_layers')
+        num_directions = 2 if bidirectional else 1
+        self.h0 = nn.Parameter(torch.zeros(num_layers * num_directions, 1, hidden_size))
+        self.c0 = nn.Parameter(torch.zeros(num_layers * num_directions, 1, hidden_size))
+        self.apply(initializer)
 
     def forward(self, input, hx=None):
         batch_size = input.shape[1]
-        h0 = self.h0.expand(-1, batch_size, -1)
-        c0 = self.c0.repeat(-1, batch_size, -1)
+        h0 = self.h0.expand(-1, batch_size, -1).contiguous()
+        c0 = self.c0.expand(-1, batch_size, -1).contiguous()
         super().forward(input, (h0, c0))
-
 
 class Listener(nn.Module):
     def __init__(self):
         super().__init__()
+        # TODO
+        self.apply(initializer)
 
     def forward(self, *input):
         pass
@@ -35,13 +39,26 @@ class Listener(nn.Module):
 class Speller(nn.Module):
     def __init__(self):
         super().__init__()
+        # TODO
+        self.apply(initializer)
 
     def forward(self, *input):
         pass
 
-class LASModel(nn.Module):
+class LASNet(nn.Module):
     def __init__(self):
         super().__init__()
+        self.Phi = nn.ModuleList([
 
-    def forward(self, *input):
+        ])
+        self.Psi = nn.ModuleList([
+
+        ])
+
+        decoder_h0 = nn.Parameter(torch.zeros(1, ))
+        decoder_c0 = nn.Parameter(torch.zeros(1, ))
+        # TODO
+        self.apply(initializer)
+
+    def forward(self, input, forward=0):
         pass
