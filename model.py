7b60896805c9fd8a502fdc599a39616f5f650afe
Hang Gong <gohakido@gmail.com>
2018-04-17 01:34:49 -0400

    PBLSTM finished.

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import torch.nn.functional as F
 
+CUDA_AVAILABLE = torch.cuda.is_available()
 
 def initializer(m):
     for name, param in m.named_parameters():
@@ -10,28 +12,36 @@ def initializer(m):
         elif 'bias' in name:
             param.data.zero_()
 
-class VLSTM(nn.LSTM):
+class VLSTM(nn.Module):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        hidden_size = kwargs.get('hidden_size')
-        bidirectional = kwargs.get('bidirectional')
-        num_layers = kwargs.get('num_layers')
-        num_directions = 2 if bidirectional else 1
-        self.h0 = nn.Parameter(torch.zeros(num_layers * num_directions, 1, hidden_size))
-        self.c0 = nn.Parameter(torch.zeros(num_layers * num_directions, 1, hidden_size))
-        self.apply(initializer)
+        super().__init__()
+        self.lstm = nn.LSTM(*args, **kwargs)
+        num_directions = 2 if self.lstm.bidirectional else 1
+        self.h_0 = nn.Parameter(torch.zeros(self.lstm.num_layers * num_directions, 1, self.lstm.hidden_size))
+        self.c_0 = nn.Parameter(torch.zeros(self.lstm.num_layers * num_directions, 1, self.lstm.hidden_size))
 
-    def forward(self, input, hx=None):
-        batch_size = input.shape[1]
-        h0 = self.h0.expand(-1, batch_size, -1).contiguous()
-        c0 = self.c0.expand(-1, batch_size, -1).contiguous()
-        super().forward(input, (h0, c0))
+    def forward(self, seqs, seq_lens):
+        batch_size = seqs.shape[1]
+        h_0 = self.h_0.expand(-1, batch_size, -1).contiguous()
+        c_0 = self.c_0.expand(-1, batch_size, -1).contiguous()
+        h = pack_padded_sequence(seqs, seq_lens, batch_first=self.lstm.batch_first)
+        h, _ = self.lstm(h, (h_0, c_0))
+        return pad_packed_sequence(h, batch_first=self.lstm.batch_first)
+
+class PBLSTM(VLSTM):
+    def forward(self, seqs, seq_lens):
+        seqs, seq_lens = super().forward(seqs, seq_lens)
+        if seqs.shape[0] % 2 == 1:  # odd number of timestamps
+            seqs = seqs[:-1]        # remove the last frame
+        seqs = seqs.transpose(0, 1) # N, L, C
+        N, L, C = seqs.shape
+        seqs = seqs.view(N, L / 2, C * 2).transpose(0, 1).contiguous()
+        return seqs, seq_lens / 2
 
 class Listener(nn.Module):
     def __init__(self):
         super().__init__()
         # TODO
-        self.apply(initializer)
 
     def forward(self, *input):
         pass
@@ -40,12 +50,11 @@ class Speller(nn.Module):
     def __init__(self):
         super().__init__()
         # TODO
-        self.apply(initializer)
 
     def forward(self, *input):
         pass
 
-class LASNet(nn.Module):
+class LASModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.Phi = nn.ModuleList([
